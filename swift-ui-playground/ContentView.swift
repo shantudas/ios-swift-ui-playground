30c5b896af87f62d46a5530455a99daeb5a48d00
Shantu Chandra Das <shantuchandardas@Shantus-MacBook-Pro.local>
2022-01-12 13:31:34 +0600

    Hstack implemented

--- swift-ui-playground/ContentView.swift
+++ swift-ui-playground/ContentView.swift
@@ -9,8 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack{
+            Text("Hello, world!")
+                .padding()
+            Text("Hello, from Earth")
+                .padding()
+        }
+        
     }
 }
 
